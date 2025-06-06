e1143d060de0cd57db74a28d5b5911d1994b60b6
Sr. Kobama <125419153+Mikael-Kobama@users.noreply.github.com>
2025-06-05 22:14:44 -0300

    create

---
+++ 05_rent-calculator.py
@@ -0,0 +1,8 @@
+def calcular_lucro(inv_inicial, dias_inv, retorno_inv):
+
+  retorno_diario = retorno_inv / 100 / dias_inv
+
+  lucro_diario = dias_inv * retorno_inv
+  lucro_semanal = 0
+  lucro_mensal = 0
+  lucro_total = 0
